84ff9604223081d206abf73adba2eaa98b740f81
Dao Thanh Phu <21097761.phu@student.iuh.edu.vn>
2025-03-14 20:20:32 +0700

    login and sign up

--- Server/services/MessageService.js
+++ Server/services/MessageService.js
@@ -1,19 +1,25 @@
-const Message = require("../models/Message");
+const Message = require("../Models/Message");
 
-const message = require("../models/Message");
+const message = require("../Models/Message");
 
-exports.sendMessage = async(senderId, recieverId, content, messageType="text", fileUrl = null) => {
-    try {
-        const newMessage = new Message({
-            sender: senderId,
-            receiver: recieverId,
-            content,
-            messageType,
-            fileUrl
-        });
-        await newMessage.save();
-        return { message: "Message sent", status: 201, data: newMessage };
-    } catch (error) {
-        return { error: error.message, status: 500 };
-    }
-}
+exports.sendMessage = async (
+  senderId,
+  recieverId,
+  content,
+  messageType = "text",
+  fileUrl = null
+) => {
+  try {
+    const newMessage = new Message({
+      sender: senderId,
+      receiver: recieverId,
+      content,
+      messageType,
+      fileUrl,
+    });
+    await newMessage.save();
+    return { message: "Message sent", status: 201, data: newMessage };
+  } catch (error) {
+    return { error: error.message, status: 500 };
+  }
+};
